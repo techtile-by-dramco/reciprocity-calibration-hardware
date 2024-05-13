4e0cb0104756f7bfbe2154e8ec1bc86c105f14c6
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2024-05-13 10:51:10 +0200

    Update example.py

--- software/example.py
+++ software/example.py
@@ -1,5 +1,8 @@
+# example script to lock PLL to freq
+
 import pll
 import time 
+import argparse
 
 p = pll.PLL()
 
@@ -7,8 +10,26 @@ p.set_LED_mode(pll.LED_MODE_LOCK_DETECT)
 
 p.power_on()
 p.enable_output()
-p.frequency(500)
 
-while not p.locked():
-	print("Not locked")
-	time.sleep(0.5)
+
+def main():
+	parser = argparse.ArgumentParser(description="Set up PLL at freq in MHz (multiple of 10MHz)")
+	parser.add_argument("--freq", required=True, type=int, help="Frequency (multiple of 10MHz)")
+
+	args = parser.parse_args()
+	freq = args.freq
+	print(f"Frequency {freq}MHz")
+
+	assert freq%10==0, "Frequency should be a muliple of 10MHz"
+
+	p.frequency(freq)
+	
+	print("locking")
+	while not p.locked():
+		print(".", endl="")
+		time.sleep(0.5)
+	
+	print("\nLocked")
+
+if __name__ == "__main__":
+    main()
