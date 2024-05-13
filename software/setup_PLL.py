84f686f113ed305462cd177094b428b3e3ff5313
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-13 10:54:37 +0200

    Update setup_PLL.py

--- software/setup_PLL.py
+++ software/setup_PLL.py
@@ -2,35 +2,38 @@
 # usage setup_PLL.py --freq 920
 
 import pll
-import time 
+import time
 import argparse
-
-p = pll.PLL()
-
-p.set_LED_mode(pll.LED_MODE_LOCK_DETECT)
-
-p.power_on()
-p.enable_output()
 
 
 def main():
-	parser = argparse.ArgumentParser(description="Set up PLL at freq in MHz (multiple of 10MHz)")
-	parser.add_argument("--freq", required=True, type=int, help="Frequency (multiple of 10MHz)")
+    parser = argparse.ArgumentParser(
+        description="Set up PLL at freq in MHz (multiple of 10MHz)")
+    parser.add_argument("--freq", required=True, type=int,
+                        help="Frequency (multiple of 10MHz)")
 
-	args = parser.parse_args()
-	freq = args.freq
-	print(f"Frequency {freq}MHz")
+    p = pll.PLL()
 
-	assert freq%10==0, "Frequency should be a muliple of 10MHz"
+    p.set_LED_mode(pll.LED_MODE_LOCK_DETECT)
 
-	p.frequency(freq)
-	
-	print("locking")
-	while not p.locked():
-		print(".", endl="")
-		time.sleep(0.5)
-	
-	print("\nLocked")
+    p.power_on()
+    p.enable_output()
+
+    args = parser.parse_args()
+    freq = args.freq
+    print(f"Frequency {freq}MHz")
+
+    assert freq % 10 == 0, "Frequency should be a muliple of 10MHz"
+
+    p.frequency(freq)
+
+    print("locking")
+    while not p.locked():
+        print(".", endl="")
+        time.sleep(0.5)
+
+    print("\nLocked")
+
 
 if __name__ == "__main__":
     main()
